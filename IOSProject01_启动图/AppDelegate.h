6dd0701ae9f3ea88dc94006a8d48feb55020401c
Sun <nanfengwuheng@qq.com>
2018-06-25 09:57:44 +0800

    Initial Commit

---
+++ IOSProject01_启动图/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSProject01_启动图
+//
+//  Created by Experimental Computer on 2018/6/25.
+//  Copyright © 2018年 Luxondata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOSProject01_启动图/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSProject01_启动图
+//
+//  Created by Experimental Computer on 2018/6/25.
+//  Copyright © 2018年 Luxondata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
